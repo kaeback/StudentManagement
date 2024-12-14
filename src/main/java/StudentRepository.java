af029e90b27e6cb0e267f8bf4fc1df033c2d6a12
최승호 <kaeback@gmail.com>
2024-12-14 13:37:58 +0900

    StudentRepository 인터페이스 정의

--- src/main/java/StudentRepository.java
+++ src/main/java/StudentRepository.java
@@ -1,50 +1,14 @@
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
-import java.io.InputStream;
 import java.util.List;
 
-class StudentRepository {
-    private final SqlSessionFactory sqlSessionFactory;
-
-    public StudentRepository() {
-        InputStream inputStream = StudentManagementApp.class.getResourceAsStream("mybatis-config.xml");
-        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-    }
-
-    public List<Student> findAll() {
-        try (SqlSession session = sqlSessionFactory.openSession()) {
-            StudentMapper mapper = session.getMapper(StudentMapper.class);
-            return mapper.findAll();
-        }
-    }
-
-    public Student findById(int id) {
-        try (SqlSession session = sqlSessionFactory.openSession()) {
-            StudentMapper mapper = session.getMapper(StudentMapper.class);
-            return mapper.findById(id);
-        }
-    }
-
-    public void save(Student student) {
-        try (SqlSession session = sqlSessionFactory.openSession(true)) {
-            StudentMapper mapper = session.getMapper(StudentMapper.class);
-            mapper.save(student);
-        }
-    }
-
-    public void update(Student student) {
-        try (SqlSession session = sqlSessionFactory.openSession(true)) {
-            StudentMapper mapper = session.getMapper(StudentMapper.class);
-            mapper.update(student);
-        }
-    }
-
-    public void delete(int id) {
-        try (SqlSession session = sqlSessionFactory.openSession(true)) {
-            StudentMapper mapper = session.getMapper(StudentMapper.class);
-            mapper.delete(id);
-        }
-    }
+interface StudentRepository {
+    // 모든 학생정보 조회
+    List<Student> findAll();
+    // 학생 ID로 학생정보 조회
+    Student findById(int id);
+    // 학생정보 저장
+    void save(Student student);
+    // 학생정보 수정
+    void update(Student updatedStudent);
+    // 학생정보 삭제
+    void delete(int id);
 }
---
+++ src/main/java/StudentRepositoryImpl.java
@@ -0,0 +1,50 @@
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+import java.util.List;
+
+class StudentRepositoryImpl implements StudentRepository {
+    private final SqlSessionFactory sqlSessionFactory;
+
+    public StudentRepositoryImpl() {
+        InputStream inputStream = StudentManagementApp.class.getResourceAsStream("mybatis-config.xml");
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+    public List<Student> findAll() {
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            StudentMapper mapper = session.getMapper(StudentMapper.class);
+            return mapper.findAll();
+        }
+    }
+
+    public Student findById(int id) {
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            StudentMapper mapper = session.getMapper(StudentMapper.class);
+            return mapper.findById(id);
+        }
+    }
+
+    public void save(Student student) {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            StudentMapper mapper = session.getMapper(StudentMapper.class);
+            mapper.save(student);
+        }
+    }
+
+    public void update(Student student) {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            StudentMapper mapper = session.getMapper(StudentMapper.class);
+            mapper.update(student);
+        }
+    }
+
+    public void delete(int id) {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            StudentMapper mapper = session.getMapper(StudentMapper.class);
+            mapper.delete(id);
+        }
+    }
+}
